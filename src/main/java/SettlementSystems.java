518855415bc0ec4ac4cdb856eecf7a8cf2b2ca01
leutnant <karl.shtolz.army@gmail.com>
2023-06-06 21:45:46 +0300

    Initial

---
+++ src/main/java/SettlementSystems.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SettlementSystems {
+    private static final String ERROR_EMPTY_ARRAY = " ERROR! EMPTY_ARRAY! ";
+    private static final String START_PARSING_ARR = ">>START_PARSING ARRAY = ";
+    private static final String START_PARSING_ARR_TAG = "<<";
+    private static final String END_PARSING_ARR = ">>END_PARSING<<";
+    private static final String FLAG_CONTAIN_EQUALS = "+";
+    private static final int COLUMN_STRING = 0;
+    private static final int COLUMN_EQUAL_INDEX = 1;
+    private static final int COLUMN_FLAG_EQUAL = 2;
+    private static final int LEN_ALL_CHAR_ARRAY = 1500;
+    private static final int COLUMN_COUNT = 3;
+    private static final String FLAG_CONTINUE = ">";
+    private static final String FLAG_NOW_IN_PROCESS = "-";
+    private static final String FLAG_NO_INFO = "?";
+    private static final String EQUAL_CHAR = " = ";
+    private static final String DELIMITER = ", ";
+    public ArrayList<String> findEqualsString (String [] arrString) {
+        ArrayList<String> returnedList = new ArrayList<>();
+        if (arrString == null || arrString.length == 0) {
+            System.out.println(ERROR_EMPTY_ARRAY);
+            returnedList.add(ERROR_EMPTY_ARRAY);
+            return returnedList;
+        }
+        String [][] answer = new String[arrString.length][COLUMN_COUNT];
+        System.out.println(START_PARSING_ARR + Arrays.toString(arrString) + START_PARSING_ARR_TAG);
+        for (int i = 0; i < arrString.length; i++) {
+            answer[i][COLUMN_STRING] = arrString[i];
+            answer[i][COLUMN_EQUAL_INDEX] = EQUAL_CHAR + i;
+            answer[i][COLUMN_FLAG_EQUAL] = FLAG_NOW_IN_PROCESS;
+            for (int j = arrString.length - 1; j > i; j--) {
+                if (answer[j][COLUMN_FLAG_EQUAL] == null) {
+                    answer[j][COLUMN_FLAG_EQUAL] = FLAG_NO_INFO;
+                }
+                if (isContainEqualsChars(arrString[i], arrString[j]) && !answer[j][COLUMN_FLAG_EQUAL].equals(FLAG_CONTINUE)) {
+                    String tmp = answer[i][COLUMN_EQUAL_INDEX] + DELIMITER + j;
+                    answer[i][COLUMN_EQUAL_INDEX] = tmp;
+                    answer[i][COLUMN_FLAG_EQUAL] = FLAG_CONTAIN_EQUALS;
+                    answer[j][COLUMN_FLAG_EQUAL] = FLAG_CONTINUE;
+                }
+            }
+        }
+        printArray(answer);
+        for (int i = 0; i < arrString.length; i++) {
+            if (answer[i][COLUMN_FLAG_EQUAL] != null && answer[i][COLUMN_FLAG_EQUAL].equals(FLAG_CONTAIN_EQUALS)) {
+                returnedList.add(answer[i][COLUMN_STRING] + answer[i][COLUMN_EQUAL_INDEX]);
+            }
+        }
+        System.out.println(END_PARSING_ARR);
+        return returnedList;
+    }
+    public boolean isContainEqualsChars (String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() != str2.length()) {
+            return false;
+        }
+        int [] arrCharAsInt = new int[LEN_ALL_CHAR_ARRAY];
+        char [] strAsArr1 = str1.toCharArray();
+        char [] strAsArr2 = str2.toCharArray();
+        for (int i = 0; i < strAsArr1.length; i++) {
+            int charAsInt1 = strAsArr1[i];
+            int charAsInt2 = strAsArr2[i];
+            arrCharAsInt[charAsInt1] += 1;
+            arrCharAsInt[charAsInt2] -= 1;
+        }
+        for (int i = 0; i < LEN_ALL_CHAR_ARRAY; i++) {
+            if (arrCharAsInt[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void printArray(String [][] answer) {
+        for (String[] strings : answer) {
+            if (strings[COLUMN_FLAG_EQUAL] != null && strings[COLUMN_FLAG_EQUAL].equals(FLAG_CONTAIN_EQUALS)) {
+                System.out.println(strings[COLUMN_STRING] + strings[COLUMN_EQUAL_INDEX]);
+            }
+        }
+    }
+}
---
+++ src/test/java/SettlementSystemsTest.java
@@ -0,0 +1,78 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SettlementSystemsTest {
+    SettlementSystems settlementSystems = new SettlementSystems();
+    @Test
+    public void findEqualsStringAllStringIsEmpty() {
+        String [] test = new String[]{"","","","","","",""};
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[ = 0, 6, 5, 4, 3, 2, 1]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringNormalData() {
+        String [] test = new String[]{"a","aa","a","qwe","qwe","qwwe","q","a","aa","a","qwe","qwe","qwwe","q","a","aa","a","qwe","qwe","qwwe","q"};
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[a = 0, 16, 14, 9, 7, 2, aa = 1, 15, 8, qwe = 3, 18, 17, 11, 10, 4, qwwe = 5, 19, 12, q = 6, 20, 13]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringNormalData2() {
+        String [] test = new String[]{"a","aa","a","qwe","qwe","qwwe","q","a","aa","a","qwe","ewq","qwwe","q","a","aa","a","qwe","qwe","qwwe","q"};
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[a = 0, 16, 14, 9, 7, 2, aa = 1, 15, 8, qwe = 3, 18, 17, 11, 10, 4, qwwe = 5, 19, 12, q = 6, 20, 13]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringNormalData3() {
+        String [] test = new String[]{"a","aa","a","wasd","wdsa","wa","q","a","aa","a","qwe","ewq","qwwe","q","a","ewqw","a","qwe","qwe","qwwe","q"};
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[a = 0, 16, 14, 9, 7, 2, aa = 1, 8, wasd = 3, 4, q = 6, 20, 13, qwe = 10, 18, 17, 11, qwwe = 12, 19, 15]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringWithDigits() {
+        String [] test = new String[]{"a","123","a","wasd","wdsa","wa","231","a","aa","a","qwe","ewq","qwwe","q","a","ewqw","a","213","qwe","qwwe","q"};//qwe->ewq
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[a = 0, 16, 14, 9, 7, 2, 123 = 1, 17, 6, wasd = 3, 4, qwe = 10, 18, 11, qwwe = 12, 19, 15, q = 13, 20]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringEmptyArray() {
+        String [] test = new String[]{};
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[ ERROR! EMPTY_ARRAY! ]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringNullArray() {
+        String [] test = null;
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[ ERROR! EMPTY_ARRAY! ]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringNormalDataAndEmptyString() {
+        String [] test = new String[]{"a","aa","","qwe","qwe","","q","a","aa","","qwe","","qwwe","","a","aa","a","qwe","qwe","qwwe","q"};
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[a = 0, 16, 14, 7, aa = 1, 15, 8,  = 2, 13, 11, 9, 5, qwe = 3, 18, 17, 10, 4, q = 6, 20, qwwe = 12, 19]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringSpecialSymbols() {
+        String [] test = new String[]{"a","123","a","wasd","z-x-a-0/*!@#$%^&*()","wa","231","a","aa","/*!@#$%^&*()0-a-z-x","qwe","ewq","qwwe","q","a","ewqw","a","213","qwe","qwwe","q"};//qwe->ewq
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[a = 0, 16, 14, 7, 2, 123 = 1, 17, 6, z-x-a-0/*!@#$%^&*() = 4, 9, qwe = 10, 18, 11, qwwe = 12, 19, 15, q = 13, 20]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+    @Test
+    public void findEqualsStringRussianLetters() {
+        String [] test = new String[]{"л","аа","ф","аопр","рп","","нгок","ф","аа","й","одл","","лод","","л","рпоа","а","апр","пра","когн","й"};
+        String actualAnswerAsString = settlementSystems.findEqualsString(test).toString();
+        String expectedAnswerAsString = "[л = 0, 14, аа = 1, 8, ф = 2, 7, аопр = 3, 15,  = 5, 13, 11, нгок = 6, 19, й = 9, 20, одл = 10, 12, апр = 17, 18]";
+        Assert.assertEquals(expectedAnswerAsString, actualAnswerAsString);
+    }
+}
